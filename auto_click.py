7e6fbdaef6dc16321332ae23e1330736aabdc8f7
Joshua Shen <m18217152127@outlook.com>
2023-02-25 15:33:21 +0800

    big change

--- auto_click.py
+++ auto_click.py
@@ -2,18 +2,21 @@ from pymouse import PyMouse #调用Pymouse模块
 from PIL import Image
 from PIL import ImageChops
 import time #调用时间模块（考略到网站点击延时）
-print("LTH1")
-def click_func(loop_time, sleep_duation):
-    print("LTH")
-    n = loop_time
-    t = sleep_duation
+
+def click_func():
+    print("Start program")
+    n = 1000
+    t = 1
     for i in range(n): #循环n次
         m = PyMouse()
         a = m.position()#获取当前坐标的位置
         print(i)
         print(a) #显示位置
         time.sleep(t)   #延时t秒
-        if compare_images("screenshot.png") == "success":
+        print("grapping screen")
+        grap_screen()
+        print("grapping screen done")
+        if compare_images() == "success":
             m.move(100, 100) #鼠标移动至坐标（x,y）
             a = m.position()
             print(a)
@@ -24,18 +27,20 @@ def click_func(loop_time, sleep_duation):
 
 
 def grap_screen():
-    im2 = ImageGrab.grab(bbox =(0, 0, 300, 300))
+    area = (0, 0, 300, 300)
+    im2 = ImageGrab.grab(area)
+    im2.save("screenshot.png")
     return im2
 
-def compare_images(path_one):
+def compare_images():
     """
     比较图片
     :param path_one: 第一张图片的路径
     :param path_two: 指定屏幕位置
     :return: 相同返回 success
     """
-    image_one = Image.open(path_one)
-    image_two = grap_screen()
+    image_one = Image.open("screenshot.png")
+    image_two = Image.open("STANDER.jpg")
     try:
         diff = ImageChops.difference(image_one, image_two)
 
@@ -50,8 +55,5 @@ def compare_images(path_one):
 
 
 if __name__ == '__main__':
-    click_func(
-        100, 
-        1
-    )
+    click_func()
 
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
-print("LTH")
+from PIL import Image
+from PIL import ImageChops
+
+print("LTH Initialize")
+
+area = (0, 0, 300, 300)
+im2 = ImageGrab.grab(area)
+im2.save("STANDER.jpg")
